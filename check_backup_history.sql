efbf2cf39a6146a981100bc28eb5ae0c2e342f14
Marcelo Adade <17789164+sqlsrvgeek@users.noreply.github.com>
2019-12-24 10:26:52 -0300

    Update check_backup_history.sql

--- check_backup_history.sql
+++ check_backup_history.sql
@@ -18,5 +18,4 @@ FROM msdb.dbo.backupmediafamily
 INNER JOIN msdb.dbo.backupset ON msdb.dbo.backupmediafamily.media_set_id = msdb.dbo.backupset.media_set_id 
 WHERE (CONVERT(datetime, msdb.dbo.backupset.backup_start_date, 102) >= GETDATE() - 7) 
 ORDER BY 
-msdb.dbo.backupset.database_name, 
-msdb.dbo.backupset.backup_finish_date 
+msdb.dbo.backupset.backup_finish_date desc
